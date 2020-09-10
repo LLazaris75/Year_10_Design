df1587270e2ff4ba6e711585072c22edaf0bd5a8
Luca Lazaris <luca.lazaris@GCCYJPP0J1WK.local>
2020-09-10 10:58:09 -0400

    First Work

---
+++ Tools/ToolsLL.py
@@ -0,0 +1,13 @@
+def isEven(a):
+
+	if a % 2 == 0:
+		return True
+
+	return False
+
+print(isEven(0))
+print(isEven(10))
+print(isEven(9))
+
+for i in range(0, 100, 1)
+	print(str(i)+" is "+str(isEven(i)))
